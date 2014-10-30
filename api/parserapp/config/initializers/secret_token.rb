c0bea955aded335ca5fdc7594d0e0fd0a4c50093
andrewwhe <andrew@andrewhe.me>
2014-10-30 17:52:46 -0400

    added ruby app

---
+++ api/parserapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parserapp::Application.config.secret_token = 'ed32d667510a2cd2c89de01e541d74cc87029e599160bf596f65b25f3f08078a7416d314e2fa070592f5549e59e791f64a571d0c4a7a54b9c8c0eac9c3c07824'
